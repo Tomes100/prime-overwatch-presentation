1e63b30d0905644a1a59138ff7f880df7064793e
Prime Mac <tomescnt100@gmail.com>
2025-09-02 01:34:31 +0300

    github blob

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<video src="https://drive.google.com/uc?export=download&id=1LhOTX7cMP6P_tFjvuXJaQwD2pDNkeQWx" controls preload="metadata" style="width:100%;height:auto;max-width:100vw;">
+<video src="https://github.com/Tomes100/prime-overwatch-presentation/releases/download/v1.0.0/Prime.Overwatch.mp4" controls preload="metadata" style="width:100%;height:auto;max-width:100vw;">
     <track kind="captions" src="/presentation.vtt" srclang="en" label="English" default />
 </video>
