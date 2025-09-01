8cef0e35670cd1b429ed38b4edc823cb67bc76c6
Prime Mac <tomescnt100@gmail.com>
2025-09-02 02:03:19 +0300

    Included prime-cloud redirect

---
+++ src/routes/prime-cloud/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+ let seconds = 5;
+
+ if (typeof window !== 'undefined') {
+  const intervalId = setInterval(() => {
+   seconds -= 1;
+   if (seconds <= 0) {
+    clearInterval(intervalId);
+    window.location.href = '/';
+   }
+  }, 1000);
+ }
+</script>
+
+<div style="min-height:60vh;display:flex;align-items:center;justify-content:center;font-size:2rem;">
+ Redirecting to Prime Overwatch in {seconds}s...
+</div>
+
+
