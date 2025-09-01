a5183766e4a0723c1f75def37ca72fed6fef7eb2
Prime Mac <tomescnt100@gmail.com>
2025-09-02 01:17:08 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<video src="/presentation.mp4" controls preload="metadata" style="width:100%;height:auto;max-width:100vw;">
+    <track kind="captions" src="/presentation.vtt" srclang="en" label="English" default />
+</video>
