28820d78d0ec9590239d4e97a16fe15526136291
Prime Mac <tomescnt100@gmail.com>
2025-09-02 01:29:05 +0300

    Link to gdrive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<video src="/presentation.mp4" controls preload="metadata" style="width:100%;height:auto;max-width:100vw;">
+<video src="https://drive.google.com/uc?export=download&id=1LhOTX7cMP6P_tFjvuXJaQwD2pDNkeQWx" controls preload="metadata" style="width:100%;height:auto;max-width:100vw;">
     <track kind="captions" src="/presentation.vtt" srclang="en" label="English" default />
 </video>
